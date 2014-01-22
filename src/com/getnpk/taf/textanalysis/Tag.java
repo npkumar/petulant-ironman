a708c2d4753176316c240031484d8daa511795ae
getnpk <getnpk@gmail.com>
2014-01-22 11:19:18 +0800

    Tag implementation

---
+++ src/com/getnpk/taf/textanalysis/Tag.java
@@ -0,0 +1,8 @@
+package com.getnpk.taf.textanalysis;
+
+public interface Tag {
+
+	public String getDisplayText();
+	public String getStemmedText();
+	
+}
---
+++ src/com/getnpk/taf/textanalysis/tag/TagImpl.java
@@ -0,0 +1,56 @@
+package com.getnpk.taf.textanalysis.tag;
+
+import com.getnpk.taf.textanalysis.Tag;
+
+public class TagImpl implements Tag {
+
+	private String displayText;
+	private String stemmedText;
+	
+	private int hashCode;
+	
+	//use as an immutable object.
+	public TagImpl(String displayText, String stemmedText){
+		this.displayText = displayText;
+		this.stemmedText = stemmedText;
+		this.hashCode = stemmedText.hashCode();
+	}
+	
+	@Override
+	public String getDisplayText() {
+		return null;
+	}
+
+	@Override
+	public String getStemmedText() {
+		return null;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		return this.hashCode;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TagImpl other = (TagImpl) obj;
+		if (this.hashCode != other.hashCode)
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TagImpl [displayText=" + displayText + ", stemmedText="
+				+ stemmedText + ", hashCode=" + hashCode + "]";
+	}
+	
+
+}

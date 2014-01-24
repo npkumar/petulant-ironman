a840dc4edd998d9eeceecf0334a70d6974452083
getnpk <getnpk@gmail.com>
2014-01-24 14:46:13 +0800

    extended stopword list from file

--- src/com/getnpk/taf/lucene/impl/PorterStemStopWordAnalyzer.java
+++ src/com/getnpk/taf/lucene/impl/PorterStemStopWordAnalyzer.java
@@ -1,9 +1,13 @@
 package com.getnpk.taf.lucene.impl;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.LowerCaseFilter;
@@ -17,12 +21,41 @@ import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 public class PorterStemStopWordAnalyzer extends Analyzer {
 
-	public static final String[] stopWords = { "be", "in", "and", "of", "to",
+	public  static String[] stopWords = { "be", "in", "and", "of", "to",
 			"the", "that", "then", "there", "if", "was", "am", "all", "is",
 			"this", "other", "there"};
 
-	private Set<String> stopSet;
-
+	private List<String> stopWordList;
+	
+	public PorterStemStopWordAnalyzer(){
+		stopWordList = new ArrayList<String>();
+		
+		String stopWordFile = getClass().getClassLoader().getResource("stopwords.txt").getFile();
+		File file = new File(stopWordFile);
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(file));
+			String word = null;
+			while((word = reader.readLine()) != null){
+				stopWordList.add(word);
+				word = reader.readLine();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				reader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		stopWords = new String[stopWordList.size()];
+		stopWordList.toArray(stopWords);
+		
+		
+	}
+	
 	@Override
 	public TokenStream tokenStream(String fieldName, Reader reader) {
 		// stopSet = new HashSet<String>();
